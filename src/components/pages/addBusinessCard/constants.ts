90e01788ee718c8e0c6473ea4182e1411a29b0f8
swapnil kumar <swapnil.k@goodworker.in>
2023-04-17 19:14:24 +0530

    fix keyboard

--- src/components/pages/addBusinessCard/constants.ts
+++ src/components/pages/addBusinessCard/constants.ts
@@ -1,10 +1,10 @@
 import {Schema} from "../../../utils/schema";
 
 export const INPUT_FIELDS: {key: keyof Schema, placeholder?:string, extraParams?: Record<string, string | number>}[] = [
-    { key: 'name', placeholder: 'Name' },
-    { key: 'occupation', placeholder: 'Occupation / Title' },
-    { key: 'company', placeholder: 'Company' },
-    { key: 'email', placeholder: 'Email Address', extraParams: {keyBoardType: 'email-address'} },
-    { key: 'phone', placeholder: 'Phone Number 10 digits', extraParams: { keyBoardType: 'phone-pad', maxLength: 10} },
-    { key: 'linkedin', placeholder: 'LinkedIn URL (optional)', extraParams: {keyBoardType: 'url'} },
+    { key: 'name', placeholder: 'Name', extraParams: { keyboardType: 'email-address', maxLength: 10}  },
+    { key: 'occupation', placeholder: 'Occupation / Title', extraParams: { keyboardType: 'email-address', maxLength: 10}  },
+    { key: 'company', placeholder: 'Company', extraParams: { keyboardType: 'email-address', maxLength: 10}  },
+    { key: 'email', placeholder: 'Email Address', extraParams: {keyboardType: 'email-address', maxLength: 10} },
+    { key: 'phone', placeholder: 'Phone Number 10 digits', extraParams: { keyboardType: 'phone-pad', maxLength: 10} },
+    { key: 'linkedin', placeholder: 'LinkedIn URL (optional)', extraParams: { keyboardType: 'url', maxLength: 10} },
 ];
